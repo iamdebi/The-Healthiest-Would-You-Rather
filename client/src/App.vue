87afba50fd65f59ac278475f90f6dc5d473caf9e
Debbie <debiurquhart@live.co.uk>
2020-01-10 15:53:07 +0000

    adds show and hide answer functionality

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,27 @@
 <template lang="html">
-<div id="app">
-    <p>app page</p>
- </div>
+ <div id="app">
+     <div class="QA" v-if="currentQuestion < 8">
+  
+  <div class="question" v-if="show">
+    <p>Q{{currentQuestion + 1}}</p>
+    <img src="./assets/logo.png" v-on:click="show = !show"/>
+    <img src="./assets/logo.png" v-on:click="show = !show"/>
+    </div>
+    <div class="answer" v-else>
+    <p >Answer</p>
+    <button v-on:click="currentQuestion +=1, show = !show">
+    Next!
+  </button>
+    </div>
+    
+</div>
+ <div class="summary" v-else>
+<p>Summary of User data</p>
+</div>
+
+
+
+</div>
 
 </template>
 
@@ -14,7 +34,9 @@ export default {
   data() {
     return {
       questions: [],
-      users: []
+      users: [],
+      show: true,
+      currentQuestion: 0
     };
   },
 
@@ -26,4 +48,11 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
