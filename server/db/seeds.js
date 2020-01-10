0f2a058958bd0a37cee1ce3b0654657474ff9de0
user <user@users-MacBook-Pro-9.local>
2020-01-10 12:32:57 +0000

    Added single question

---
+++ server/db/seeds.js
@@ -0,0 +1,24 @@
+use healthyquiz;
+
+db.dropDatabase();
+
+db.questions.insertMany([
+    {
+        option1: "20g of ice cream",
+        option2: "40g of butter",
+        url1: "@/assets/ice-cream.jpg",
+        url2: "@/assets/butter.jpg",
+        totalresponses1: 0,
+        totalresponses2: 0
+    },
+
+]);
+
+db.users.insertMany([
+    {
+        responses: [1,2,1,1,2,2,1,1],
+        latitude: "55.865426",
+        longitude: "-4.257861"
+    },
+
+]);
