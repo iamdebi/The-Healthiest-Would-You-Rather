c90102280ff7770022147124ce0d1e3337549329
chad-tung <sven.tung@gmail.com>
2020-01-11 16:22:37 +0000

    Fixes seeds file to be responseText

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -6,7 +6,7 @@ db.dropDatabase();
 db.questions.insertMany([{
     option1: "250ml of ice cream",
     option2: "44g of butter",
-    reponseText: "250ml of ice cream and 44g of butter have the same amount of fat",
+    responseText: "250ml of ice cream and 44g of butter have the same amount of fat",
     url1: "http://localhost:8080/img/ice-cream.jpg",
     url2: "http://localhost:8080/img/butter.jpg",
     totalResponses1: 0,
@@ -16,7 +16,7 @@ db.questions.insertMany([{
   {
     option1: "1 can of coke",
     option2: "7 sugar cubes",
-    reponseText: "1 can of coke is the same as eating 7 sugar cubes",
+    responseText: "1 can of coke is the same as eating 7 sugar cubes",
     url1: "@/assets/ice-cream.jpg",
     url2: "@/assets/butter.jpg",
     totalResponses1: 0,
@@ -26,7 +26,7 @@ db.questions.insertMany([{
   {
     option1: "1 serving of ready made pasta sauce",
     option2: "1.5 teaspoons of salt",
-    reponseText: "1 serving of ready made pasta sauce contains 1.5g of salt",
+    responseText: "1 serving of ready made pasta sauce contains 1.5g of salt",
     url1: "@/assets/ice-cream.jpg",
     url2: "@/assets/butter.jpg",
     totalResponses1: 0,
@@ -36,7 +36,7 @@ db.questions.insertMany([{
   {
     option1: "1 portion of cooked lentils",
     option2: "A hamburger",
-    reponseText: "55g of cooked lentils has nearly 18g of protein, more than the amount in a hamburger ",
+    responseText: "55g of cooked lentils has nearly 18g of protein, more than the amount in a hamburger",
     url1: "@/assets/ice-cream.jpg",
     url2: "@/assets/butter.jpg",
     totalResponses1: 0,
@@ -46,7 +46,7 @@ db.questions.insertMany([{
   {
     option1: "A blueberry muffins",
     option2: "A slice of wholemeal bread",
-    reponseText: "1 blueberry muffin has the same fibre content as a slice of wholemeal bread",
+    responseText: "1 blueberry muffin has the same fibre content as a slice of wholemeal bread",
     url1: "@/assets/ice-cream.jpg",
     url2: "@/assets/butter.jpg",
     totalResponses1: 0,
@@ -56,7 +56,7 @@ db.questions.insertMany([{
   {
     option1: "8oz ribeye stake",
     option2: "1 boiled egg",
-    reponseText: "Both options have around the same amount of cholesterol, however the cholesterol found within a boiled egg is better for your heart",
+    responseText: "Both options have around the same amount of cholesterol, however the cholesterol found within a boiled egg is better for your heart",
     url1: "@/assets/ice-cream.jpg",
     url2: "@/assets/butter.jpg",
     totalResponses1: 0,
@@ -66,7 +66,7 @@ db.questions.insertMany([{
   {
     option1: "McDonalds double quarter pounder with cheese meal with fries and a banana milkshake",
     option2: "3 avocados",
-    reponseText: "The calorie content of both of these is the same",
+    responseText: "The calorie content of both of these is the same",
     url1: "@/assets/ice-cream.jpg",
     url2: "@/assets/butter.jpg",
     totalResponses1: 0,
