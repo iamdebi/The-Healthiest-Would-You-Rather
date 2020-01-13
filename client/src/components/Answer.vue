71d3909bd60fc87eaac03e10dadb201f567bd7d5
chad-tung <sven.tung@gmail.com>
2020-01-13 11:43:15 +0000

    Adds placeholder images

--- client/src/components/Answer.vue
+++ client/src/components/Answer.vue
@@ -6,7 +6,6 @@
     <p> Percentage1: {{percentage1}}</P>
     <p> Percentage2: {{percentage2}}</P>
     <button type="button" class="next-btn" v-on:click="handleNextClick">Next</button>
-    <div id="chart_div"></div>
   </div>
 </template>
 
@@ -39,42 +38,6 @@ export default {
 
 }};
 </script>
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-<script type="text/javascript">
-
-  // Load the Visualization API and the corechart package.
-  google.charts.load('current', {'packages':['corechart']});
-
-  // Set a callback to run when the Google Visualization API is loaded.
-  google.charts.setOnLoadCallback(drawChart);
-
-  // Callback that creates and populates a data table,
-  // instantiates the pie chart, passes in the data and
-  // draws it.
-  function drawChart() {
-
-    // Create the data table.
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Option');
-    data.addColumn('number', 'Responses');
-    data.addRows([
-      ['Option 1', this.questions[this.currentQuestion].totalResponses1],
-      ['Option 2', this.questions[this.currentQuestion].totalResponses2]
-    ]);
-
-    // Set chart options
-    var options = {
-      'title':'How many people choose which option',
-      'width':400,
-      'height':300
-    };
-
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
-  }
-</script>
-
 
 <style lang="css" scoped>
 </style>
