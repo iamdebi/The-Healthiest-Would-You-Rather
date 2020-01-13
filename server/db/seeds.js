d377eccb10075253e75e00b95920d87c208f6c94
chad-tung <sven.tung@gmail.com>
2020-01-13 11:06:36 +0000

    Adds placeholder images

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -17,8 +17,8 @@ db.questions.insertMany([{
     option1: "1 can of coke",
     option2: "7 sugar cubes",
     responseText: "1 can of coke is the same as eating 7 sugar cubes",
-    url1: "@/assets/ice-cream.jpg",
-    url2: "@/assets/butter.jpg",
+    url1: "http://localhost:8080/img/coke.jpg",
+    url2: "http://localhost:8080/img/sugar-cubes.jpeg",
     totalResponses1: 124,
     totalResponses2: 26
   },
@@ -27,8 +27,8 @@ db.questions.insertMany([{
     option1: "1 serving of ready made pasta sauce",
     option2: "1.5 teaspoons of salt",
     responseText: "1 serving of ready made pasta sauce contains 1.5g of salt",
-    url1: "@/assets/ice-cream.jpg",
-    url2: "@/assets/butter.jpg",
+    url1: "http://localhost:8080/img/pasta-sauce.jpg",
+    url2: "http://localhost:8080/img/salt.jpg",
     totalResponses1: 132,
     totalResponses2: 18
   },
@@ -37,8 +37,8 @@ db.questions.insertMany([{
     option1: "1 portion of cooked lentils",
     option2: "A hamburger",
     responseText: "55g of cooked lentils has nearly 18g of protein, more than the amount in a hamburger",
-    url1: "@/assets/ice-cream.jpg",
-    url2: "@/assets/butter.jpg",
+    url1: "http://localhost:8080/img/lentils.jpg",
+    url2: "http://localhost:8080/img/hamburger.jpg",
     totalResponses1: 62,
     totalResponses2: 88
   },
@@ -47,8 +47,8 @@ db.questions.insertMany([{
     option1: "A blueberry muffin",
     option2: "A slice of wholemeal bread",
     responseText: "1 blueberry muffin has the same fibre content as a slice of wholemeal bread",
-    url1: "@/assets/ice-cream.jpg",
-    url2: "@/assets/butter.jpg",
+    url1: "http://localhost:8080/img/muffin.jpg",
+    url2: "http://localhost:8080/img/bread.jpeg",
     totalResponses1: 108,
     totalResponses2: 42
   },
@@ -57,8 +57,8 @@ db.questions.insertMany([{
     option1: "8oz ribeye steak",
     option2: "1 boiled egg",
     responseText: "Both options have around the same amount of cholesterol, however the cholesterol found within a boiled egg is better for your heart",
-    url1: "@/assets/ice-cream.jpg",
-    url2: "@/assets/butter.jpg",
+    url1: "http://localhost:8080/img/steak.webp",
+    url2: "http://localhost:8080/img/egg.jpg",
     totalResponses1: 94,
     totalResponses2: 66
   },
@@ -67,8 +67,8 @@ db.questions.insertMany([{
     option1: "McDonalds double quarter pounder with cheese meal with fries and a banana milkshake",
     option2: "3 avocados",
     responseText: "The calorie content of both of these is the same",
-    url1: "@/assets/ice-cream.jpg",
-    url2: "@/assets/butter.jpg",
+    url1: "http://localhost:8080/img/mcdonalds.jpg",
+    url2: "http://localhost:8080/img/avocados.jpg",
     totalResponses1: 81,
     totalResponses2: 69
   }
