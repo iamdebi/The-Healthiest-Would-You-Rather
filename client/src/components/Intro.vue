6e5c2242bb457520bdef1dd791192e032d0fd56a
Mark Devlin <sparkzilla@gmail.com>
2020-01-13 20:49:02 +0000

    Text formatting header /subhead

--- client/src/components/Intro.vue
+++ client/src/components/Intro.vue
@@ -1,9 +1,17 @@
 <template lang="html">
   <div class="intro-main">
-    <h1>Healthy Quiz</h1>
-    <p>Helping you make better food choices</p>
-    <button type="button" v-on:click="handleStartButtonClick()">Start</button>
-    <p>{{this.number}} people have taken this quiz</p>
+    <div class="intro-heading">
+       <h1>The Healthiest <br>Would you rather Quiz!</h1>
+    </div>
+    <div class="intro-subhead">
+       <p>Helping you make better food choices</p>
+    </div>
+    <div class="start-button-container">
+      <button type="button" v-on:click="handleStartButtonClick()">Start</button>
+    </div>
+    <div class="intro-people-counter">
+       <p>{{this.number}} people have taken this quiz</p>
+    </div>
   </div>
 </template>
 
@@ -22,12 +30,43 @@ export default {
 
 <style lang="css">
 
+@import url('https://fonts.googleapis.com/css?family=Baloo+Bhai');
+
+
 .intro-main {
   width: 900px;
   padding:20px;
   height:100vh;
   margin: 0 auto;
   background-image: linear-gradient(90deg, #0dd41a 0%, #86fc6f 100% );
+}
+
+.intro-heading {
+  margin-top: 120px;
+}
+
+.intro-heading h1 {
+
+    font-family: Baloo Bhai;
+    font-size: 72px;
+    color: #ffffff;
+    line-height: 89px;
+    text-align: center;
+}
+
+.intro-subhead {
+    margin-top:0;
+}
+
+.intro-subhead p {
+    font-family: Baloo Bhai;
+    font-size: 48px;
+    color: #e2ff05;
+    text-align: center;
+}
+
+p {
+  font-family: Open Sans;
 }
 
 </style>
