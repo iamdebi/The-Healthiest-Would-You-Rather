53f5b7f4ac704335d66e052d66c0aaf5d5a4c058
chad-tung <sven.tung@gmail.com>
2020-01-14 10:55:55 +0000

    adds functionality to move paginated dots

---
+++ client/src/components/Dots.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <li v-bind:id="[isActive ? 'active' : 'notActive']"></li>
+</template>
+
+<script>
+export default {
+  name:"dots",
+  props: ["number", "counter"],
+  computed: {
+    isActive: function(){
+      if (this.number == this.counter) {
+        return true
+      }
+      else {
+        return false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
