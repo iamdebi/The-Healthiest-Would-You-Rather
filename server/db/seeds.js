795a76d4206681ce2972cc6c4deb6929a91baaa4
Mark Devlin <sparkzilla@gmail.com>
2020-01-14 23:53:22 +0000

    New images and seeds

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -7,18 +7,18 @@ db.questions.insertMany([{
     option1: "250ml of ice cream",
     option2: "44g of butter",
     responseText: "250ml of ice cream and 44g of butter have the same amount of fat",
-    url1: "http://localhost:8080/img/ice-cream.jpg",
-    url2: "http://localhost:8080/img/butter.jpg",
+    url1: "http://localhost:8080/img/ice-cream.png",
+    url2: "http://localhost:8080/img/butter.png",
     totalResponses1: 148,
     totalResponses2: 2
   },
 
   {
-    option1: "7 sugar cubes",
+    option1: "Seven sugar cubes",
     option2: "A can of coke",
-    responseText: "Drinking a can of coke is the same as eating 7 sugar cubes",
-    url1: "http://localhost:8080/img/sugar-cubes.jpeg",
-    url2: "http://localhost:8080/img/coke.jpg",
+    responseText: "Drinking a can of coke is the same as eating seven sugar cubes",
+    url1: "http://localhost:8080/img/sugar-cubes.png",
+    url2: "http://localhost:8080/img/can-of-coke.png",
     totalResponses1: 24,
     totalResponses2: 126
   },
@@ -26,9 +26,9 @@ db.questions.insertMany([{
   {
     option1: "A pasta sauce serving",
     option2: "1.5 teaspoons of salt",
-    responseText: "A serving of ready made pasta sauce contains 1.5g of salt",
-    url1: "http://localhost:8080/img/pasta-sauce.jpg",
-    url2: "http://localhost:8080/img/salt.jpg",
+    responseText: "A serving of ready-made pasta sauce contains 1.5g of salt",
+    url1: "http://localhost:8080/img/pasta-sauce.png",
+    url2: "http://localhost:8080/img/salt.png",
     totalResponses1: 144,
     totalResponses2: 6
   },
@@ -36,9 +36,9 @@ db.questions.insertMany([{
   {
     option1: "A slice of wholemeal bread",
     option2: "A blueberry muffin",
-    responseText: "1 blueberry muffin has the same fibre content as a slice of wholemeal bread",
-    url1: "http://localhost:8080/img/bread.jpeg",
-    url2: "http://localhost:8080/img/muffin.jpg",
+    responseText: "One blueberry muffin has the same fibre content as a slice of wholemeal bread",
+    url1: "http://localhost:8080/img/bread.png",
+    url2: "http://localhost:8080/img/muffin.png",
     totalResponses1: 52,
     totalResponses2: 98
   },
@@ -46,19 +46,19 @@ db.questions.insertMany([{
   {
     option1: "A boiled egg",
     option2: "8oz ribeye steak",
-    responseText: "Both options have around the same amount of cholesterol, however the cholesterol found within a boiled egg is better for your heart",
-    url1: "http://localhost:8080/img/egg.jpg",
-    url2: "http://localhost:8080/img/steak.webp",
+    responseText: "Both options have around the same amount of cholesterol",
+    url1: "http://localhost:8080/img/egg.png",
+    url2: "http://localhost:8080/img/steak.png",
     totalResponses1: 84,
     totalResponses2: 76
   },
 
   {
-    option1: "A double quarter pounder meal and milkshake",
-    option2: "3 avocados",
+    option1: "A double quarter-pounder meal and milkshake",
+    option2: "Three avocados",
     responseText: "The calorie content of both of those options are the same",
-    url1: "http://localhost:8080/img/mcdonalds.jpg",
-    url2: "http://localhost:8080/img/avocados.jpg",
+    url1: "http://localhost:8080/img/mcdonalds.png",
+    url2: "http://localhost:8080/img/avocado.png",
     totalResponses1: 81,
     totalResponses2: 69
   }
