3360239ae4d7b36d8c0d65a06e24d728a72cd3ce
Debbie <debiurquhart@live.co.uk>
2020-01-15 10:22:41 +0000

    fixes screenshot button bug

--- client/src/components/Summary.vue
+++ client/src/components/Summary.vue
@@ -1,17 +1,19 @@
 <template lang="html">
   <div class="summary-main">
-    <div class="summary-heading">
-      <h1>Your choices!</h1>
-    </div>
-    <div class="summary-list-container">
-      <summary-list-item v-for="(question, index) in this.questions" :question="question" :responses="responses" :counter="index"/>
-    </div>
-    <div class="share-text">
-      <p>Share your results...</p>
-    </div>
-    <div class="sum-btn">
-      <share-button />
-      <button class="btn" type="button" id="screenshot" v-on:click="handleScreenshot()">Screenshot</button>
+    <div class="summary-container">
+      <div class="summary-heading">
+        <h1>Your choices!</h1>
+      </div>
+      <div class="summary-list-container">
+        <summary-list-item v-for="(question, index) in this.questions" :question="question" :responses="responses" :counter="index"/>
+      </div>
+      <div class="share-text">
+        <p>Share your results...</p>
+      </div>
+      <div class="sum-btn">
+        <share-button />
+        <button class="btn" type="button" id="screenshot" v-on:click="handleScreenshot()">Screenshot</button>
+      </div>
     </div>
   </div>
 </template>
@@ -76,7 +78,7 @@ export default {
     padding:60px 30px 30px 30px;
     height:100vh;
     margin: 0 auto;
-    background-image: linear-gradient(to right, rgba(13,212,26,.62), rgba(134,252,111,0.62));
+    background-image: linear-gradient(to right, rgba(13,212,26,0.62), rgba(134,252,111,0.62));
   }
 
   .fade-enter-active, .fade-leave-active {
